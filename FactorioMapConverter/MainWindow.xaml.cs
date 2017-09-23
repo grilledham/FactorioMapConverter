06bcfc8a003aad47e88355c264c605155dc6fabf
James Gillham <jrdgillham@gmail.com>
2017-09-23 07:06:51 +0100

    Add project files.

---
+++ FactorioMapConverter/MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace FactorioMapConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<TileData> tiles;
+        private List<TileData> enabledTiles;
+        private string imageFile;
+        private string outputFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            ProcessImageButton.IsEnabled = false;
+            ProcessImageProgress.Visibility = Visibility.Hidden;
+
+            tiles = TileData.Tiles;
+
+            TilesView.ItemsSource = tiles;
+        }
+
+        private void GetImageFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() is true)
+            {
+                imageFile = dlg.FileName;
+                ImageFilePathTextBlock.Text = imageFile;
+            }
+
+            ProcessImageButton.IsEnabled = CanProcessImage();
+        }
+
+        private string lastOutputFile = "";
+        private void SetOutputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.InitialDirectory = lastOutputFile;
+            dlg.FileName = "map.lua";
+            dlg.AddExtension = true;
+            dlg.DefaultExt = ".lua";
+            dlg.Filter = "lua file|*.lua";
+            
+
+            if (dlg.ShowDialog() is true)
+            {
+                outputFile = dlg.FileName;
+                OutputFilePathTextBlock.Text = outputFile;
+                Debug.WriteLine(System.IO.Path.GetDirectoryName(dlg.FileName));
+                lastOutputFile = System.IO.Path.GetDirectoryName(dlg.FileName);
+            }
+
+            ProcessImageButton.IsEnabled = CanProcessImage();
+        }
+
+        private bool CanProcessImage()
+        {
+            return imageFile != null && outputFile != null;
+        }
+
+        private async void ProcessImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageFile == null)
+            {
+                Debug.WriteLine("image file not set");
+                return;
+            }
+
+            if (outputFile == null)
+            {
+                Debug.WriteLine("output file not set");
+                return;
+            }
+
+            ProcessImageProgress.Visibility = Visibility.Visible;
+            ProcessImageProgress.IsIndeterminate = true;
+            ProcessImageButton.IsEnabled = false;
+            GetImageFileButton.IsEnabled = false;
+            SetOutputFileButton.IsEnabled = false;
+
+            Func<int, int, int, int, TileData> tileGetter;
+            if (BlackAndWhiteRadioButton.IsChecked is true)
+                tileGetter = GetTrueOrFalseTile;
+            else if (ColorRadioButton.IsChecked is true)
+                tileGetter = GetTileNameOfColor;
+            else
+            {
+                Debug.WriteLine("picture mode not selected");
+                return;
+            }
+
+            enabledTiles = tiles.Where(x => x.Enabled).ToList();
+            
+            string data = await Task.Run(() => DecodeImageAndCompress(imageFile, tileGetter));
+
+            using (FileStream SourceStream = File.Open(outputFile, FileMode.Create))
+            {
+                SourceStream.Seek(0, SeekOrigin.Begin);
+                await SourceStream.WriteAsync(Encoding.ASCII.GetBytes(data), 0, data.Length);
+            }
+
+            ProcessImageProgress.Visibility = Visibility.Hidden;
+            ProcessImageProgress.IsIndeterminate = false;
+            ProcessImageButton.IsEnabled = true;
+            GetImageFileButton.IsEnabled = true;
+            SetOutputFileButton.IsEnabled = true;
+        }
+
+        private string DecodeImageAndCompress(string file, Func<int, int, int, int, TileData> tileGetter)
+        {
+            BitmapImage image = new BitmapImage(new Uri(file, UriKind.Absolute));
+
+            FormatConvertedBitmap dec = new FormatConvertedBitmap();
+            dec.BeginInit();
+            dec.Source = image;
+            dec.DestinationFormat = PixelFormats.Bgra32;
+            dec.EndInit();
+
+            int height = dec.PixelHeight;
+            int width = dec.PixelWidth;
+            int stride = width * 4;
+            var bytes = new byte[height * width * 4];
+            dec.CopyPixels(bytes, stride, 0);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("return {");
+            sb.Append("height = ").Append(height).AppendLine(",");
+            sb.Append("width = ").Append(width).AppendLine(",");
+            sb.AppendLine("data = {");
+
+            for (int i = 0; i < height; i++)
+            {
+                sb.Append("\t{");
+
+                TileData prev = null;
+                int count = 0;
+
+                for (int j = 0; j < width; j++)
+                {
+                    int k = (i * width + j) * 4;
+                    byte b = bytes[k];
+                    byte g = bytes[k + 1];
+                    byte r = bytes[k + 2];
+                    byte a = bytes[k + 3];
+
+                    var tile = tileGetter(r, g, b, a);
+
+                    if (prev == null)
+                    {
+                        prev = tile;
+                        count = 1;
+                    }
+                    else if (prev == tile)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        sb.Append(prev.Number).Append(',').Append(count).Append(',');
+                        prev = tile;
+                        count = 1;
+                    }
+                }
+                if (prev != null)
+                {
+                    sb.Append(prev.Number).Append(',').Append(count).Append(',');
+                }
+
+                sb.AppendLine("},");
+            }
+
+            sb.AppendLine("}");
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        private TileData GetTileNameOfColor(int r, int g, int b, int a)
+        {
+            // https://stackoverflow.com/questions/4057475/rounding-colour-values-to-the-nearest-of-a-small-set-of-colours
+
+            int min = int.MaxValue;
+            TileData minTile = null;
+            foreach (var tile in enabledTiles)
+            {
+                var c = tile.Color;
+
+                int rd = c.R - r;
+                int gd = c.G - g;
+                int bd = c.B - b;
+
+                int dd = rd * rd + gd * gd + bd * bd;
+
+                if (dd < min)
+                {
+                    min = dd;
+                    minTile = tile;
+                }
+            }
+
+            return minTile;
+        }
+
+        private static bool IsBlack(int r, int g, int b, int a)
+        {
+            double y = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+            return y < 128;
+        }
+
+        private static TileData GetTrueOrFalseTile(int r, int b, int g, int a)
+        {
+            return IsBlack(r, g, b, a) ? TileData.Tiles[0] : TileData.Tiles[1];
+        }
+    }
+
+
+}
---
+++ FactorioMapConverter/TileData.cs
@@ -0,0 +1,60 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace FactorioMapConverter
+{
+    public class TileData : ObservableObject
+    {
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+
+        public int Number { get; set; }
+
+        private Color color;
+        public Color Color
+        {
+            get => color;
+            set => Set(ref color, value);
+        }
+
+        private bool enabled;
+        public bool Enabled
+        {
+            get => enabled;
+            set => Set(ref enabled, value);
+        }
+
+        public static List<TileData> Tiles { get; } = new List<TileData>
+        {
+            new TileData() {name = "false", Number = 1, color = Color.FromArgb(255,0,0,0), enabled = false},
+            new TileData() {name = "true", Number = 2, color = Color.FromArgb(255,255,255,255), enabled = false},
+            new TileData() {name = "concrete", Number = 3, color = Color.FromArgb(255,105,104,102), enabled = true},
+            new TileData() {name = "deepwater", Number = 4, color = Color.FromArgb(255,24,71,86), enabled = true},
+            new TileData() {name = "deepwater-green", Number = 5, color = Color.FromArgb(255,24,38,14), enabled = true},
+            new TileData() {name = "dirt", Number = 6, color = Color.FromArgb(255,154,112,43), enabled = true},
+            new TileData() {name = "dirt-dark", Number = 7, color = Color.FromArgb(255,130,92,34), enabled = true},
+            new TileData() {name = "grass", Number = 8, color = Color.FromArgb(255,77,64,22), enabled = true},
+            new TileData() {name = "grass-dry", Number = 9, color = Color.FromArgb(255,75,56,25), enabled = true},
+            new TileData() {name = "grass-medium", Number = 10, color = Color.FromArgb(255,65,57,20), enabled = true},
+            new TileData() {name = "lab-dark-1", Number = 11, color = Color.FromArgb(255,44,44,44), enabled = true},
+            new TileData() {name = "lab-dark-2", Number = 12, color = Color.FromArgb(255,63,63,63), enabled = true},
+            new TileData() {name = "out-of-map", Number = 13, color = Color.FromArgb(255,0,0,0), enabled = true},
+            new TileData() {name = "red-desert", Number = 14, color = Color.FromArgb(254,125,90,68), enabled = true},
+            new TileData() {name = "red-desert-dark", Number = 15, color = Color.FromArgb(255,116,74,59), enabled = true},
+            new TileData() {name = "sand", Number = 16, color = Color.FromArgb(255,177,144,65), enabled = true},
+            new TileData() {name = "sand-dark", Number = 17, color = Color.FromArgb(255,161,126,49), enabled = true},
+            new TileData() {name = "stone-path", Number = 18, color = Color.FromArgb(255,76,77,74), enabled = true},
+            new TileData() {name = "water", Number = 19, color = Color.FromArgb(255,23,90,107), enabled = true},
+            new TileData() {name = "water-green", Number = 20, color = Color.FromArgb(255,29,47,15), enabled = true},
+        };
+    }
+}
